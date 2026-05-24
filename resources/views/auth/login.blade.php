5a006eac8a827ad1f88474ce7b20eaf94d8a750f
Febrianti Dewi Lestari <febriantidlestari@gmail.com>
2026-05-24 14:12:44 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,20 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="flex items-center justify-between mt-4">
+    @if (Route::has('password.request'))
+        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md" href="{{ route('password.request') }}">
+            {{ __('Forgot your password?') }}
+        </a>
+    @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+    <a class="text-sm text-gray-600 hover:text-gray-900 font-bold" href="{{ route('register') }}">
+        Belum punya akun? Daftar sekarang!
+    </a>
+
+    <x-primary-button class="ms-4">
+        {{ __('Log in') }}
+    </x-primary-button>
+</div>
     </form>
 </x-guest-layout>
